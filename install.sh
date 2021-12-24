5429d0dc1196181d82c1805c9daeda09173b9b60
alpha2phi <alpha2phi@gmail.com>
2021-12-24 21:54:25 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+
+TARGET_DIR=~/.config/nvim-beginner/nvim
+
+mkdir -p $TARGET_DIR
+
+stow --restow --target=$TARGET_DIR .
+
+
---
+++ lua/plugins.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+local packer_bootstrap = false
+
+local function packer_init()
+  local fn = vim.fn
+  local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system {
+      "git",
+      "clone",
+      "--depth",
+      "1",
+      "https://github.com/wbthomason/packer.nvim",
+      install_path,
+    }
+    vim.cmd [[packadd packer.nvim]]
+  end
+  vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+end
+
+packer_init()
+
+function M.setup()
+  local conf = {
+    compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+    },
+  }
+
+  local function plugins(use)
+    use { "wbthomason/packer.nvim" }
+    use { "lewis6991/impatient.nvim" }
+
+    if packer_bootstrap then
+      print "Setting up Neovim. Restart required after installation!"
+      require("packer").sync()
+    end
+  end
+
+  require("packer").init(conf)
+  require("packer").startup(plugins)
+end
+
+return M
