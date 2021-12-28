a5bfd3dc30399649d63cfe254e377e790265d562
alpha2phi <alpha2phi@gmail.com>
2021-12-28 09:22:41 +0800

    Initial commit

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -1,18 +1,18 @@
 local keymap = vim.api.nvim_set_keymap
-local default_options = { noremap = true, silent = true }
-local expr_options = { noremap = true, expr = true, silent = true }
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
 
 -- Center search results
-keymap("n", "n", "nzz", default_options)
-keymap("n", "N", "Nzz", default_options)
+keymap("n", "n", "nzz", default_opts)
+keymap("n", "N", "Nzz", default_opts)
 
 -- Visual line wraps
-keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_options)
-keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_options)
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
 
 -- Better indent
-keymap("v", "<", "<gv", default_options)
-keymap("v", ">", ">gv", default_options)
+keymap("v", "<", "<gv", default_opts)
+keymap("v", ">", ">gv", default_opts)
 
 -- paste over currently selected text without yanking it
-keymap("v", "p", '"_dP', default_options)
+keymap("v", "p", '"_dP', default_opts)
