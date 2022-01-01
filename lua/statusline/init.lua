347d6fc3cdad58a0e5ada32af096cfe555ac1d72
alpha2phi <alpha2phi@gmail.com>
2022-01-01 16:05:27 +0800

    Initial commit

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -1,7 +1,24 @@
-local M = {}
+local function status_line()
+  local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
+  local file_name = "%-.16t"
+  local buf_nr = "[%n]"
+  local modified = " %-m"
+  local file_type = " %y"
+  local right_align = "%="
+  local line_no = "%10([%l/%L%)]"
+  local pct_thru_file = "%5p%%"
 
-function M.setup()
-  print "testing"
+  return string.format(
+    "%s%s%s%s%s%s%s%s",
+    mode,
+    file_name,
+    buf_nr,
+    modified,
+    file_type,
+    right_align,
+    line_no,
+    pct_thru_file
+  )
 end
 
-return M
+vim.opt.statusline = status_line()
