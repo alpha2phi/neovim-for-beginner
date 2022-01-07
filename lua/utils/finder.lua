cf8210b044ddb6bbf43772818fd46fba519ee7ce
alpha2phi <alpha2phi@gmail.com>
2022-01-07 21:09:09 +0800

    Initial commit

--- lua/utils/finder.lua
+++ lua/utils/finder.lua
@@ -3,12 +3,10 @@ local M = {}
 function M.find_files()
   local fzf = require "fzf-lua"
   if vim.fn.system "git rev-parse --is-inside-work-tree" == true then
-    fzf.files()
-  else
     fzf.git_files()
+  else
+    fzf.files()
   end
 end
-
-M.find_files()
 
 return M
