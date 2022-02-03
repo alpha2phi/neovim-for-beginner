91b3b7f3c45bb0eeb7fb9d4c67c5f681a68fde69
alpha2phi <alpha2phi@gmail.com>
2022-02-03 22:36:09 +0800

    Initial commit

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -6,7 +6,29 @@ local servers = {
   jsonls = {},
   pyright = {},
   rust_analyzer = {},
-  sumneko_lua = {},
+  sumneko_lua = {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+          -- Setup your lua path
+          path = vim.split(package.path, ";"),
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          },
+        },
+      },
+    },
+  },
   tsserver = {},
   vimls = {},
 }
