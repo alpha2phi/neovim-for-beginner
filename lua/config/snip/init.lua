84201b7119a617d9590fc40977547f36cdd3001b
alpha2phi <alpha2phi@gmail.com>
2022-02-11 12:29:16 +0800

    Update snips

--- lua/config/snip/init.lua
+++ lua/config/snip/init.lua
@@ -26,7 +26,7 @@ local function create_snippets()
       s("ttt", t "Testing Luasnip"),
     },
     lua = {
-      s("lm", { t "local M = {}", t "function M.setup()", i(1, ""), t "end", t "return M" }),
+      s("lm", { t { "local M = {}", "", "function M.setup()", "" }, i(1, ""), t { "", "end", "", "return M" } }),
     },
   }
 end
