11673a44672e635e6fade24f3f8b796fd9b79cd6
alpha2phi <alpha2phi@gmail.com>
2022-03-04 22:43:51 +0800

    Update snippets

--- lua/config/snip/init.lua
+++ lua/config/snip/init.lua
@@ -52,6 +52,9 @@ function M.setup()
   require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_snipmate").lazy_load()
 
+  -- Load custom javascript
+  require("luasnip.loaders.from_vscode").load { paths = { "./snippets/javascript" } }
+
   ls.filetype_extend("all", { "_" })
 
   -- Create new snippets
