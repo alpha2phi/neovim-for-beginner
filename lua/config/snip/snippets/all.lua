b3160d9145d6010e2d7d1fbdb614fdadca538d8e
alpha2phi <alpha2phi@gmail.com>
2022-03-12 13:00:36 +0800

    Initial commit

--- lua/config/snip/snippets/all.lua
+++ lua/config/snip/snippets/all.lua
@@ -1,6 +1,6 @@
 local ls = require "luasnip"
 local s = ls.snippet
-local t = ls.text_node
+-- local t = ls.text_node
 
 -- local i = ls.insert_node
 -- local c = ls.choice_node
@@ -9,12 +9,12 @@ local t = ls.text_node
 -- local fmt = require("luasnip.extras.fmt").fmt
 -- local events = require "luasnip.util.events"
 -- local types = require "luasnip.util.types"
--- local f = ls.function_node
+local f = ls.function_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
 -- local l = require("luasnip.extras").lambda
 -- local rep = require("luasnip.extras").rep
--- local p = require("luasnip.extras").partial
+local p = require("luasnip.extras").partial
 -- local m = require("luasnip.extras").match
 -- local n = require("luasnip.extras").nonempty
 -- local dl = require("luasnip.extras").dynamic_lambda
@@ -22,8 +22,21 @@ local t = ls.text_node
 -- local fmta = require("luasnip.extras.fmt").fmta
 -- local conds = require "luasnip.extras.expand_conditions"
 
+local function bash(_, snip)
+  local file = io.popen(snip.trigger, "r")
+  local res = {}
+  for line in file:lines() do
+    table.insert(res, line)
+  end
+  return res
+end
+
 local snippets = {
-  s("todo", t "TODO"),
+  s({ trig = "ymd", name = "Current date", dscr = "Insert the current date" }, {
+    p(os.date, "%Y-%m-%d"),
+  }),
+
+  s({ trig = "pwd" }, { f(bash, {}) }),
 }
 
 return snippets
--- lua/config/snip/snippets/python.lua
+++ lua/config/snip/snippets/python.lua
@@ -1,9 +1,13 @@
 local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 local snippets = {
-  s("todo", t "print('TODO')"),
+  s("shebang", {
+    t { "#!/usr/bin/env python", "" },
+    i(0),
+  }),
 }
 
 return snippets
