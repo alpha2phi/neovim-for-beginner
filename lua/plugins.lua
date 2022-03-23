75af6274778844e9746f072ea16f75da6947a6ae
alpha2phi <alpha2phi@gmail.com>
2022-03-23 22:39:07 +0800

    Update plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,6 +67,7 @@ function M.setup()
     use {
       "sainnhe/everforest",
       config = function()
+        vim.g.everforest_better_performance = 1
         vim.cmd "colorscheme everforest"
       end,
       disable = false,
