83dfd1b8cd1083920883dc18fb1e3dd676ff2299
alpha2phi <alpha2phi@gmail.com>
2022-04-17 23:28:38 +0800

    Update snippets

--- lua/config/snip/snippets/all.lua
+++ lua/config/snip/snippets/all.lua
@@ -5,11 +5,12 @@ local i = ls.insert_node
 local c = ls.choice_node
 local sn = ls.snippet_node
 -- local isn = ls.indent_snippet_node
--- local fmt = require("luasnip.extras.fmt").fmt
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 -- local events = require "luasnip.util.events"
 -- local types = require "luasnip.util.types"
 local f = ls.function_node
--- local d = ls.dynamic_node
+local d = ls.dynamic_node
 -- local r = ls.restore_node
 -- local l = require("luasnip.extras").lambda
 -- local rep = require("luasnip.extras").rep
@@ -37,8 +38,62 @@ local snippets = {
   }),
 
   s({ trig = "pwd" }, { f(bash, {}) }),
-
   s("choice", { c(1, { t "choice 1", t "choice 2", t "choice 3" }) }),
+
+  s(
+    "dt",
+    f(function()
+      return os.date "%D - %H:%M"
+    end)
+  ),
+
+  s(
+    "fmt2",
+    fmt(
+      [[
+    foo({1}, {3}) {{
+        return {2} * {4}
+      }}
+    ]],
+      {
+        i(1, "x"),
+        rep(1),
+        i(2, "y"),
+        rep(2),
+      }
+    )
+  ),
+
+  s("yy", p(os.date, "%Y")),
+
+  s("link_url", {
+    t '<a href="',
+    f(function(_, snip)
+      -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+      -- In this case only the first line is inserted.
+      return snip.env.TM_SELECTED_TEXT[1] or {}
+    end, {}),
+    t '">',
+    i(1),
+    t "</a>",
+    i(0),
+  }),
+
+  s("ddd", {
+    t "from: ",
+    i(1),
+    t { "", "to: " },
+    d(2, function(args)
+      -- the returned snippetNode doesn't need a position; it's inserted
+      -- "inside" the dynamicNode.
+      return sn(nil, {
+        -- jump-indices are local to each snippetNode, so restart at 1.
+        i(1, args[1]),
+      })
+    end, {
+      1,
+    }),
+  }),
 }
 
 local autosnippets = {
--- lua/config/snip/snippets/lua.lua
+++ lua/config/snip/snippets/lua.lua
@@ -4,6 +4,9 @@ local t = ls.text_node
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
+local l = require("luasnip.extras").lambda
+local f = ls.function_node
+
 -- local c = ls.choice_node
 -- local sn = ls.snippet_node
 -- local isn = ls.indent_snippet_node
@@ -11,8 +14,36 @@ local rep = require("luasnip.extras").rep
 local snippets = {
   ls.parser.parse_snippet("lm", "local M = {}\n\nfunction M.setup()\n  $1 \nend\n\nreturn M"),
   -- s("lm", { t { "local M = {}", "", "function M.setup()", "" }, i(1, ""), t { "", "end", "", "return M" } }),
-  s("todo", t 'print("TODO")'),
-  s("req", fmt("local {} = require('{}')", { i(1, "default"), rep(1) })),
-}
 
+  s("todo", t 'print("TODO")'),
+
+  -- s("req", fmt("local {} = require('{}')", { i(1, "default"), rep(1) })),
+  s(
+    "localreq1",
+    fmt('local {} = require("{}")', {
+      l(l._1:match("[^.]*$"):gsub("[^%a]+", "_"), 1),
+      i(1, "module"),
+    })
+  ),
+  s(
+    "localreq2",
+    fmt([[local {} = require "{}"]], {
+      f(function(import_name)
+        local parts = vim.split(import_name[1][1], ".", true)
+        return parts[#parts] or ""
+      end, {
+        1,
+      }),
+      i(1),
+    })
+  ),
+
+  s(
+    "preq",
+    fmt('local {1}_ok, {1} = pcall(require, "{}")\nif not {1}_ok then return end', {
+      l(l._1:match("[^.]*$"):gsub("[^%a]+", "_"), 1),
+      i(1, "module"),
+    })
+  ),
+}
 return snippets
