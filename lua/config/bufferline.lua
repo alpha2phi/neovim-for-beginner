9ab16dafa4cb6150d713b9241c24154c109ba556
alpha2phi <alpha2phi@gmail.com>
2022-04-23 18:34:40 +0800

    Update bufferline

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -3,12 +3,14 @@ local M = {}
 function M.setup()
   require("bufferline").setup {
     options = {
-      numbers = "none",
+      mode = "buffers",
+      numbers = "buffer_id",
       diagnostics = "nvim_lsp",
       separator_style = "slant" or "padded_slant",
       show_tab_indicators = true,
       show_buffer_close_icons = false,
       show_close_icon = false,
+      color_icons = true,
     },
   }
 end
