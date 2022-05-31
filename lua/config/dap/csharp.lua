249ff1b5f247ab34e27d9cc1150e1fb4d1debc2c
alpha2phi <alpha2phi@gmail.com>
2022-05-31 17:47:20 +0800

    Initial commit

---
+++ lua/config/dap/csharp.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+-- Debugger installation location
+local HOME = os.getenv "HOME"
+local DEBUGGER_LOCATION = HOME .. "/.local/share/nvim/netcoredbg"
+
+function M.setup()
+  local dap = require "dap"
+
+  -- Adapter configuration
+  dap.adapters.coreclr = {
+    type = "executable",
+    command = DEBUGGER_LOCATION .. "/netcoredbg",
+    args = { "--interpreter=vscode" },
+  }
+
+
+  -- Configuration
+  dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to DLL > ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end,
+  },
+}
+end
+
+return M
--- lua/config/dap/init.lua
+++ lua/config/dap/init.lua
@@ -55,6 +55,7 @@ local function configure_debuggers()
   require("config.dap.python").setup()
   require("config.dap.rust").setup()
   require("config.dap.go").setup()
+  require("config.dap.csharp").setup()
 end
 
 function M.setup()
