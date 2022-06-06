1291db02c8739473313d9c160ae4177fc1786be5
alpha2phi <alpha2phi@gmail.com>
2022-06-06 18:53:07 +0800

    Update kotlin debug adapter

--- lua/config/dap/kotlin.lua
+++ lua/config/dap/kotlin.lua
@@ -2,30 +2,18 @@ local M = {}
 
 -- Debugger installation location
 local HOME = os.getenv "HOME"
-local DEBUGGER_LOCATION = HOME .. "/.local/share/nvim/netcoredbg"
+local DEBUGGER_LOCATION = HOME .. "/.local/share/nvim/kotlin-debug-adapter"
 
 function M.setup()
   local dap = require "dap"
 
   -- Adapter configuration
-  dap.adapters.kotlin = {
-    type = "executable",
-    command = DEBUGGER_LOCATION .. "/netcoredbg",
-    args = { "--interpreter=vscode" },
-  }
-
+  dap.adapters.kotlin = {}
 
   -- Configuration
   dap.configurations.kotlin = {
-  {
-    type = "coreclr",
-    name = "launch - netcoredbg",
-    request = "launch",
-    program = function()
-        return vim.fn.input('Path to DLL > ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-    end,
-  },
-}
+    {},
+  }
 end
 
 return M
