acbb3800fa9ffd9c3b0e121d3eee679c683829b7
alpha2phi <alpha2phi@gmail.com>
2022-06-09 14:10:16 +0800

    Update dap config

--- lang-servers/install-kotlin-debugger.sh
+++ lang-servers/install-kotlin-debugger.sh
@@ -2,7 +2,7 @@
 
 INSTALL_FOLDER="$HOME/.local/share/nvim"
 
-# Install java-debug
+# Install kotlin-debug-adapter
 rm -rf "$INSTALL_FOLDER/kotlin-debug-adapter"
 git clone https://github.com/fwcd/kotlin-debug-adapter.git "$INSTALL_FOLDER/kotlin-debug-adapter"
 cd "$INSTALL_FOLDER/kotlin-debug-adapter" && ./gradlew :adapter:installDist
--- lua/config/dap/kotlin.lua
+++ lua/config/dap/kotlin.lua
@@ -22,18 +22,19 @@ function M.setup()
       request = "launch",
       projectRoot = vim.fn.getcwd() .. "/app",
       mainClass = function()
-        return vim.fn.input("Path to main class > ", "myapp.sample.app.AppKt", "file")
+        -- return vim.fn.input("Path to main class > ", "myapp.sample.app.AppKt", "file")
+        return vim.fn.input("Path to main class > ", "", "file")
       end,
     },
-    {
-      type = "kotlin",
-      name = "attach - kotlin",
-      request = "attach",
-      projectRoot = vim.fn.getcwd() .. "/app",
-      hostName = "localhost",
-      port = 5005,
-      timeout = 1000,
-    },
+    -- {
+    --   type = "kotlin",
+    --   name = "attach - kotlin",
+    --   request = "attach",
+    --   projectRoot = vim.fn.getcwd() .. "/app",
+    --   hostName = "localhost",
+    --   port = 5005,
+    --   timeout = 1000,
+    -- },
   }
 end
 
