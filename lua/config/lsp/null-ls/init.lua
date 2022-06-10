7533e15e3c119c6badf1b4f6b0742f78b1631b16
alpha2phi <alpha2phi@gmail.com>
2022-06-10 15:28:40 +0800

    Update null-ls

--- lua/config/lsp/null-ls/init.lua
+++ lua/config/lsp/null-ls/init.lua
@@ -10,13 +10,13 @@ local with_diagnostics_code = function(builtin)
   }
 end
 
-local with_root_file = function(builtin, file)
-  return builtin.with {
-    condition = function(utils)
-      return utils.root_has_file(file)
-    end,
-  }
-end
+-- local with_root_file = function(builtin, file)
+--   return builtin.with {
+--     condition = function(utils)
+--       return utils.root_has_file(file)
+--     end,
+--   }
+-- end
 
 local sources = {
   -- formatting
