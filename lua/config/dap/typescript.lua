ce0a579bf9b567e2665ae8195856fbc7bf44ae2e
alpha2phi <alpha2phi@gmail.com>
2022-06-12 22:56:09 +0800

    Initial commit

--- lua/config/dap/typescript.lua
+++ lua/config/dap/typescript.lua
@@ -1,7 +1,68 @@
 local M = {}
 
+local HOME = os.getenv "HOME"
+local DEBUGGER_LOCATION = HOME .. "/.local/share/nvim/vscode-chrome-debug"
+
 function M.setup()
-  print "Setting up typescript debugger"
+  local dap = require "dap"
+
+  dap.adapters.chrome = {
+    type = "executable",
+    command = "node",
+    args = { DEBUGGER_LOCATION .. "/out/src/chromeDebug.js" },
+  }
+
+  dap.configurations.javascript = { 
+    {
+      type = "chrome",
+      request = "attach",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      port = 9222,
+      webRoot = "${workspaceFolder}",
+    },
+  }
+
+  dap.configurations.javascriptreact = {
+    {
+      type = "chrome",
+      request = "attach",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      port = 9222,
+      webRoot = "${workspaceFolder}",
+    },
+  }
+
+  dap.configurations.typescript = {
+    {
+      type = "chrome",
+      request = "attach",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      port = 9222,
+      webRoot = "${workspaceFolder}",
+    },
+  }
+
+  dap.configurations.typescriptreact = {
+    {
+      type = "chrome",
+      request = "attach",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+      protocol = "inspector",
+      port = 9222,
+      webRoot = "${workspaceFolder}",
+    },
+  }
 end
 
 return M
