a453ebe80852ca0ded568b332620392e8ae76252
alpha2phi <alpha2phi@gmail.com>
2022-06-25 11:15:28 +0800

    Update config

--- lua/config/hydra.lua
+++ lua/config/hydra.lua
@@ -2,81 +2,49 @@ local M = {}
 
 function M.setup()
   local Hydra = require "hydra"
-  local gitsigns = require "gitsigns"
-
-  local hint = [[
-   _J_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line
-   _K_: prev hunk   _u_: undo stage hunk   _p_: preview hunk   _B_: blame show full 
-   ^ ^              _S_: stage buffer      ^ ^                 _/_: show base file
-   ^
-   ^ ^              _<Enter>_: Neogit              _q_: exit
-  ]]
-
   Hydra {
-    hint = hint,
+    hint = [[
+ ^^^^^^     Move     ^^^^^^   ^^     Split         ^^^^    Size
+ ^^^^^^--------------^^^^^^   ^^---------------    ^^^^-------------
+ ^ ^ _k_ ^ ^   ^ ^ _K_ ^ ^    _s_: horizontally    _+_ _-_: height
+ _h_ ^ ^ _l_   _H_ ^ ^ _L_    _v_: vertically      _>_ _<_: width
+ ^ ^ _j_ ^ ^   ^ ^ _J_ ^ ^    _q_: close           ^ _=_ ^: equalize
+ focus^^^^^^   window^^^^^^
+ ^ ^ ^ ^ ^ ^   ^ ^ ^ ^ ^ ^    ^ ^ ^    _<Esc>_
+]],
     config = {
-      color = "pink",
-      invoke_on_body = true,
+      -- timeout = 4000,
+      invoke_on_body = false,
       hint = {
-        position = "bottom",
         border = "rounded",
+        -- position = 'bottom',
       },
-      on_enter = function()
-        vim.bo.modifiable = false
-        gitsigns.toggle_signs(true)
-        gitsigns.toggle_linehl(true)
-      end,
-      on_exit = function()
-        gitsigns.toggle_signs(false)
-        gitsigns.toggle_linehl(false)
-        gitsigns.toggle_deleted(false)
-        vim.cmd "echo" -- clear the echo area
-      end,
     },
-    mode = { "n", "x" },
-    body = "<leader>g",
+    mode = "n",
+    body = "<C-w>",
     heads = {
-      {
-        "J",
-        function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gitsigns.next_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true },
-      },
-      {
-        "K",
-        function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gitsigns.prev_hunk()
-          end)
-          return "<Ignore>"
-        end,
-        { expr = true },
-      },
-      { "s", ":Gitsigns stage_hunk<CR>", { silent = true } },
-      { "u", gitsigns.undo_stage_hunk },
-      { "S", gitsigns.stage_buffer },
-      { "p", gitsigns.preview_hunk },
-      { "d", gitsigns.toggle_deleted, { nowait = true } },
-      { "b", gitsigns.blame_line },
-      {
-        "B",
-        function()
-          gitsigns.blame_line { full = true }
-        end,
-      },
-      { "/", gitsigns.show, { exit = true } }, -- show the base of the file
-      { "<Enter>", "<cmd>Neogit<CR>", { exit = true } },
-      { "q", nil, { exit = true, nowait = true } },
+      -- Move focus
+      { "h", "<C-w>h" },
+      { "j", "<C-w>j" },
+      { "k", "<C-w>k" },
+      { "l", "<C-w>l" },
+      -- Move window
+      { "H", "<Cmd>WinShift left<CR>" },
+      { "J", "<Cmd>WinShift down<CR>" },
+      { "K", "<Cmd>WinShift up<CR>" },
+      { "L", "<Cmd>WinShift right<CR>" },
+      -- Split
+      { "s", "<C-w>s" },
+      { "v", "<C-w>v" },
+      { "q", "<Cmd>try | close | catch | endtry<CR>", { desc = "close window" } },
+      -- Size
+      { "+", "<C-w>+" },
+      { "-", "<C-w>-" },
+      { ">", "2<C-w>>", { desc = "increase width" } },
+      { "<", "2<C-w><", { desc = "decrease width" } },
+      { "=", "<C-w>=", { desc = "equalize" } },
+      --
+      { "<Esc>", nil, { exit = true } },
     },
   }
 end
--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -40,7 +40,7 @@ local servers = {
         diagnostics = {
           -- Get the language server to recognize the `vim` global
           globals = { "vim", "describe", "it", "before_each", "after_each", "packer_plugins" },
-          disable = { "lowercase-global", "undefined-global", "unused-local", "unused-vararg", "trailing-space" },
+          -- disable = { "lowercase-global", "undefined-global", "unused-local", "unused-vararg", "trailing-space" },
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
