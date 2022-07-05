81f82b63f98ba33af3ecadc884ec315216757b8f
alpha2phi <alpha2phi@gmail.com>
2022-07-05 11:04:53 +0800

    Update project settings

--- lua/config/project.lua
+++ lua/config/project.lua
@@ -4,6 +4,8 @@ function M.setup()
   require("project_nvim").setup {
     detection_methods = { "pattern", "lsp" },
     patterns = { ".git" },
+    ignore_lsp = { "null-ls" },
+    -- silent_chdir = false,
   }
 end
 
