fabca58b7d00c57420ad252431b4dc7600af2fa9
alpha2phi <alpha2phi@gmail.com>
2022-08-21 00:08:48 +0800

    chore: comment infrequently used language servers

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -103,7 +103,7 @@ local servers = {
     },
   },
   vimls = {},
-  tailwindcss = {},
+  -- tailwindcss = {},
   yamlls = {
     schemastore = {
       enable = true,
@@ -119,25 +119,25 @@ local servers = {
   },
   jdtls = {},
   dockerls = {},
-  graphql = {},
+  -- graphql = {},
   bashls = {},
-  omnisharp = {},
-  kotlin_language_server = {},
-  emmet_ls = {},
-  marksman = {},
-  angularls = {},
-  sqls = {
-    -- settings = {
-    --   sqls = {
-    --     connections = {
-    --       {
-    --         driver = "sqlite3",
-    --         dataSourceName = os.getenv "HOME" .. "/workspace/db/chinook.db",
-    --       },
-    --     },
-    --   },
-    -- },
-  },
+  -- omnisharp = {},
+  -- kotlin_language_server = {},
+  -- emmet_ls = {},
+  -- marksman = {},
+  -- angularls = {},
+  -- sqls = {
+  -- settings = {
+  --   sqls = {
+  --     connections = {
+  --       {
+  --         driver = "sqlite3",
+  --         dataSourceName = os.getenv "HOME" .. "/workspace/db/chinook.db",
+  --       },
+  --     },
+  --   },
+  -- },
+  -- },
 }
 
 function M.on_attach(client, bufnr)
