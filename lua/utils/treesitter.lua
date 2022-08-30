720155b4355d1262e818607b36a4a5ed417bbf25
alpha2phi <alpha2phi@gmail.com>
2022-08-31 00:05:00 +0800

    feat: wip treesitter query

--- lua/utils/treesitter.lua
+++ lua/utils/treesitter.lua
@@ -1,14 +1,16 @@
 local M = {}
 
 local python_function_query_string = [[
-  (function_definition) @function
+  (function_definition 
+    name: (identifier) @name (#offset! @name)
+  ) @func
 ]]
 
 -- local ts_locals = require "nvim-treesitter.locals"
 -- local ts_utils = require "nvim-treesitter.ts_utils"
-local q = require "vim.treesitter.query"
+-- local q = require "vim.treesitter.query"
 
-local bufnr_test = 99
+local bufnr_test = 28
 
 function M.get_functions(bufnr, lang, query_string)
   local parser = vim.treesitter.get_parser(bufnr, lang)
@@ -19,12 +21,17 @@ function M.get_functions(bufnr, lang, query_string)
   -- for id, node in query:iter_captures(root, bufnr, 0, -1) do
   --   dump(q.get_node_text(node, bufnr))
   -- end
-  dump(q)
+  -- dump(q)
+  
+  local func_info = {}
 
   for _, captures, metadata in query:iter_matches(root, bufnr) do
-    dump(q.get_node_text(captures[1], bufnr))
-    dump(metadata)
+    local row, col, _ = captures[1]:start()
+    local name = vim.treesitter.query.get_node_text(captures[1], bufnr)
+    -- dump(metadata[1].range)
+    table.insert(func_info, {name, row, col})
   end
+  vim.pretty_print(func_info)
 end
 
 M.get_functions(bufnr_test, "python", python_function_query_string)
