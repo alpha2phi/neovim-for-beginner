28b12652ae2f31fe622afb72bf48abe8c5d1f3dd
alpha2phi <alpha2phi@gmail.com>
2022-09-08 22:41:42 +0800

    chore: fix keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -39,8 +39,8 @@ keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", default_opts)
 keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
 
 -- Move selected line / block of text in visual mode
-keymap("x", "K", ":move '<-2<CR>gv-gv", default_opts)
-keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+keymap("x", "K", ":move '<-2<CR>gv=gv", default_opts)
+keymap("x", "J", ":move '>+1<CR>gv=gv", default_opts)
 
 -- Resizing panes
 keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
