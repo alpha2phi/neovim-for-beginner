76812b75936b979bba5aaa5de9e6b0f8dfb7d8a0
alpha2phi <alpha2phi@gmail.com>
2022-10-04 20:52:20 +0800

    fix: lualine and noice.nvim plugin

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -112,8 +112,10 @@ function M.setup()
     options = {
       icons_enabled = true,
       theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = " ", right = "" },
+      -- component_separators = { left = "", right = "" },
+      component_separators = {},
+      -- section_separators = { left = " ", right = "" },
+      section_separators = {},
       disabled_filetypes = {
         statusline = {},
         winbar = {
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,6 +86,19 @@ function M.setup()
       end,
       disable = true,
     }
+    use {
+      "folke/noice.nvim",
+      event = "VimEnter",
+      config = function()
+        require("noice").setup()
+      end,
+      requires = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
+      },
+      disable = true,
+    }
 
     -- Colorscheme
     use {
