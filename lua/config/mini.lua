6ed3dc544078d726e6cc3b3dd7ec982cf3e2ce2a
alpha2phi <alpha2phi@gmail.com>
2022-10-16 15:47:32 +0800

    feat: mini.nvim textobjects

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -4,10 +4,11 @@ function M.setup()
   -- https://github.com/echasnovski/mini.nvim/blob/main/lua/mini/ai.lua
   require("mini.ai").setup {
     custom_textobjects = {
-      b = require("mini.ai").gen_spec.argument { brackets = { "%b()" }, "^.().*().$" },
-      B = require("mini.ai").gen_spec.argument { brackets = { "%b{}" }, "^.().*().$" },
-      r = require("mini.ai").gen_spec.argument { brackets = { "%b[]" }, "^.().*().$" },
+      b = require("mini.ai").gen_spec.argument { brackets = { "%b()", "^.%s*().-()%s*.$" } },
+      B = require("mini.ai").gen_spec.argument { brackets = { "%b{}", "^.%s*().-()%s*.$" } },
+      r = require("mini.ai").gen_spec.argument { brackets = { "%b[]", "^.%s*().-()%s*.$" } },
     },
+    search_method = "cover_or_next",
   }
   require("mini.align").setup {}
   require("mini.jump").setup {}
