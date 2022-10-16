1c928cdbb8b0fd4717599cd8e83ae2b68d36f737
alpha2phi <alpha2phi@gmail.com>
2022-10-16 15:06:38 +0800

    chore: add custom textobjects

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -1,7 +1,14 @@
 local M = {}
 
 function M.setup()
-  require("mini.ai").setup {}
+  -- https://github.com/echasnovski/mini.nvim/blob/main/lua/mini/ai.lua
+  require("mini.ai").setup {
+    custom_textobjects = {
+      b = require("mini.ai").gen_spec.argument { brackets = { "%b()" }, "^.().*().$" },
+      B = require("mini.ai").gen_spec.argument { brackets = { "%b{}" }, "^.().*().$" },
+      r = require("mini.ai").gen_spec.argument { brackets = { "%b[]" }, "^.().*().$" },
+    },
+  }
   require("mini.align").setup {}
   require("mini.jump").setup {}
 end
