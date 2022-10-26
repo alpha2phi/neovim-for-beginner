764278706a2bd40a22b8dc944caed2bd9da03885
alpha2phi <alpha2phi@gmail.com>
2022-10-26 18:31:57 +0800

    feat: debugging

--- lua/config/dap/javascript.lua
+++ lua/config/dap/javascript.lua
@@ -43,6 +43,28 @@ function M.setup()
       },
     }
   end
+
+  for _, language in ipairs { "typescriptreact", "javascriptreact" } do
+    require("dap").configurations[language] = {
+      {
+        type = "pwa-chrome",
+        name = "Attach - Remote Debugging",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-chrome",
+        name = "Launch Chrome",
+        request = "launch",
+        url = "http://localhost:3000",
+      },
+    }
+  end
 end
 
 return M
