7cde6cf2f22b8a6773971a6a5ed1f06cfcd7bfb5
alpha2phi <alpha2phi@gmail.com>
2022-11-07 12:46:33 +0800

    refactor: dap config

--- lua/config/dap/init.lua
+++ lua/config/dap/init.lua
@@ -38,7 +38,47 @@ local function configure_exts()
   }
 
   local dap, dapui = require "dap", require "dapui"
-  dapui.setup {} -- use default
+  dapui.setup {
+    expand_lines = true,
+    icons = { expanded = "", collapsed = "", circular = "" },
+    mappings = {
+      -- Use a table to apply multiple mappings
+      expand = { "<CR>", "<2-LeftMouse>" },
+      open = "o",
+      remove = "d",
+      edit = "e",
+      repl = "r",
+      toggle = "t",
+    },
+    layouts = {
+      {
+        elements = {
+          { id = "scopes", size = 0.33 },
+          { id = "breakpoints", size = 0.17 },
+          { id = "stacks", size = 0.25 },
+          { id = "watches", size = 0.25 },
+        },
+        size = 0.33,
+        position = "right",
+      },
+      {
+        elements = {
+          { id = "repl", size = 0.45 },
+          { id = "console", size = 0.55 },
+        },
+        size = 0.27,
+        position = "bottom",
+      },
+    },
+    floating = {
+      max_height = 0.9,
+      max_width = 0.5, -- Floats will be treated as percentage of your screen.
+      border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
+      mappings = {
+        close = { "q", "<Esc>" },
+      },
+    },
+  } -- use default
   dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
   end
