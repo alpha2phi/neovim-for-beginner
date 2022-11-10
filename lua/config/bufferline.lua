678047b43afc78ef008a94d6348ad022684dbe2a
alpha2phi <alpha2phi@gmail.com>
2022-11-10 16:05:18 +0800

    fix: misc tuning

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -11,6 +11,7 @@ function M.setup()
       show_buffer_close_icons = false,
       show_close_icon = false,
       color_icons = true,
+      enforce_regular_tabs = false,
       custom_filter = function(buf_number, buf_numbers)
         local tab_num = 0
         for _ in pairs(vim.api.nvim_list_tabpages()) do
